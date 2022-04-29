5408a7895987de23827af59a1111def323fa74be
Anna <annank18@gmail.com>
2022-04-30 01:00:00 +0400

    feat:done dns

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
+function getDNSStats(domains) {
+  let dom = domains.map((el) => el.split('.').reverse());
+  let array = [];
+  for (let i = 0; i < dom.length; i++) {
+    let x = '';
+    for (let j = 0; j < dom[i].length; j++) {
+      if (dom[i][j]) {
+        x = x+ ('.'+ dom[i][j])
+        array.push(x)};
+    }
+  }
+  let result = array.reduce((obj, elem) => {
+    obj[elem] = (obj[elem] || 0) + 1
+    return obj
+  }, {});
+  return result
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
 
 module.exports = {
